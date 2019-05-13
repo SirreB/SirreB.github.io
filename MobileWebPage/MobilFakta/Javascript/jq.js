29a83383f935b0aa7ea582aeada5d458b7f0aebb
SirreB <43954240+SirreB@users.noreply.github.com>
2019-05-13 14:07:32 +0200

    huge update

--- MobileWebPage/MobilFakta/Javascript/jq.js
+++ MobileWebPage/MobilFakta/Javascript/jq.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
 
 /* Ha show grejerna här, borde bara vara Navbar men kanske även Bilderna */
 
+$(".nav").show();
+$(".nav").css({"display": "flex"});
 
 
 
@@ -46,4 +48,31 @@ $("#Home").click(function(){
         navWrapper.classList.remove("scale-in-hor-left");
         navWrapper.classList.remove("show");
         navWrapper.classList.add("hide");
+  });
+
+
+  $("#datorHome").click(function(){
+
+        $('html, body').animate({
+            scrollTop: $("#main").offset()
+        }, 2000);
+  });
+
+  $("#datorUtsida").click(function(){
+
+        $('html, body').animate({
+            scrollTop: $("#ruta1").offset().top
+        }, 2000);
+  });
+  $("#datorSoC").click(function(){
+
+        $('html, body').animate({
+            scrollTop: $("#ruta2").offset().top
+        }, 2000);
+  });
+  $("#datorDisplay").click(function(){
+
+        $('html, body').animate({
+            scrollTop: $("#ruta3").offset().top
+        }, 2000);
   });
